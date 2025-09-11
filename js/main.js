1952421d0087b9df6b3e6acc014a0a45c4d5b1bf
Abdulaziz <abdulazizprogrammer777@gmail.com>
2025-09-11 11:29:13 +0500

    2-dars

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const cardTemplateEl = document.getElementById('templateCard')
+const containerEl = document.getElementById('container')
+const loadingEl = document.getElementById('loading')
+const skelatonEl = document.getElementById('skeletons')
+
+
+function init() {
+	loadingEl.style.display = "block"
+	fetch('https://json-api.uz/api/project/fn43/cars').then((res) => {
+		return res.json()
+	}).then((res) => {
+		ui(res.data)
+	}).finally(() => {
+		loadingEl.style.display = "none"
+		skelatonEl.remove()
+	})
+}
+
+
+function ui(cars) {
+	containerEl.innerHTML = ''
+	cars.forEach(element => {
+		const clone = cardTemplateEl.cloneNode(true).content
+		const titleEl = clone.querySelector('h2')
+		const descripyionEl = clone.querySelector('p')
+		const cotegoryEl = clone.querySelector('mark')
+		const deleteBtnEl = clone.querySelector('button')
+
+
+		titleEl.innerText = element.name
+		descripyionEl.innerText = element.description
+		cotegoryEl.innerText = element.category
+		deleteBtnEl.id = element.id
+		console.log(deleteBtnEl.id);
+		
+		
+		containerEl.append(clone)
+
+	});
+}
+init()
+
+document.addEventListener("click", (e) => {
+	if(e.target.classList.contains('delete-btn')) {
+		deleteEl(e.target.id)
+		
+	}
+})
+
+function deleteEl(id) {
+	fetch(`https://json-api.uz/api/project/fn43/cars/${id}`, {
+		method :"DELETE"
+	}).then((res) => {
+		init()
+		
+	}).then((res) => {
+	}).finally(() => {
+	})
+}
